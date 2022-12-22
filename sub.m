34f556d3e9aa86e8a304796cae22b07a1b89afde
w-x-xiong <73966079+w-x-xiong@users.noreply.github.com>
2022-12-22 14:05:11 +0100

    Add files via upload

---
+++ sub.m
@@ -0,0 +1,20 @@
+function x = sub(X,D)
+% Subspace algorithm
+% --------------------------------
+% x = sub(X,r);
+% x = 2D position estimate
+% X = matrix for receiver positions
+% r = TOA measurement vector
+% 
+
+%In our case, this function should be invoked as sub(anc,M), in order to 
+%output the location estimate
+
+
+Y = X';
+L = size(Y,1); % number of receivers
+
+% [U,Lamda] = eig(D);
+[U,S,V] = svd(D);
+Un = U(:,3:end);
+x = (Y'*(Un*Un')*ones(L,1))/(ones(L,1)'*(Un*Un')*ones(L,1));
